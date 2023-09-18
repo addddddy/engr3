ded215deea32e160bfc49eda6d8f379421b10fc1
addddddy <abuckne38@charlottesvilleschools.org>
2023-09-18 15:23:55 -0400

    got the code ahihihihihii

--- asdfghjk.py
+++ asdfghjk.py
@@ -1,23 +1,37 @@
-# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-"""CircuitPython Essentials Servo standard servo example"""
 import time
 import board
 import pwmio
 from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull
 
-# create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.D6, duty_cycle=2 ** 15, frequency=50)
+btn = DigitalInOut(board.D5)
+btn.direction = Direction.INPUT
+btn.pull = Pull.DOWN
 
-# Create a servo object, my_servo.
+btn2 = DigitalInOut(board.D4)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN
+pwm = pwmio.PWMOut(board.D7, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
+
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
+    if btn.value:
+        print("BTN1 is down")
+        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+            my_servo.angle = angle
+        time.sleep(0.02)
+    else:
+        print("BTN1 is up")
+        pass
+
+    if btn2.value:
+        print("BTN2 is down")
+        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+             my_servo.angle = angle
+        time.sleep(0.02)
+    else:
+        print("BTN2 is up")
+        pass
+
+    time.sleep(0.1) # sleep for debounce
